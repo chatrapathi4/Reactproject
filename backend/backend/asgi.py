4c434c96f5825af22cc087d840e0fb2de24a0d8f
Thigulla Chatrapathi Reddy <chatrapathireddythigulla@gmail.com>
2025-10-10 22:08:50 +0530

    idk

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -1,16 +1,24 @@
 import os
-from django.core.asgi import get_asgi_application
+import django
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-import whiteboard.routing
+from django.core.asgi import get_asgi_application
+from django.urls import re_path
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+django.setup()
+
+from whiteboard.consumers import UltraFastWhiteboardConsumer, UltraFastChatConsumer
+
+websocket_urlpatterns = [
+    re_path(r'ws/whiteboard/(?P<room_name>\w+)/$', UltraFastWhiteboardConsumer.as_asgi()),
+    re_path(r'ws/chat/(?P<room_name>\w+)/$', UltraFastChatConsumer.as_asgi()),
+    re_path(r'ws/ide/(?P<ide_id>\w+)/$', UltraFastChatConsumer.as_asgi()),
+]
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
-        URLRouter(
-            whiteboard.routing.websocket_urlpatterns
-        )
+        URLRouter(websocket_urlpatterns)
     ),
 })
