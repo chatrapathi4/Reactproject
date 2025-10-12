c8bfc4d6b8f33697e21888543f83de64f83a666a
Thigulla Chatrapathi Reddy <chatrapathireddythigulla@gmail.com>
2025-10-12 09:25:52 +0530

    remove chatc-all

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -3,7 +3,6 @@ from django.urls import path, re_path
 from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
-from whiteboard.react_view import ReactAppView
 
 # Import ALL the views we need
 from whiteboard.views import (
@@ -26,14 +25,12 @@ urlpatterns = [
     path('api/board/create/', create_board, name='create_board'),
     path('api/board/join/', join_board, name='join_board'),
     path('api/boards/', get_active_boards, name='get_active_boards'),
+    
+    # Serve React app only for root path
+    path('', TemplateView.as_view(template_name='index.html'), name='home'),
 ]
 
 # Add static files for development
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-# React app catch-all - MUST BE LAST, using custom view
-urlpatterns += [
-    re_path(r'^.*$', ReactAppView.as_view(), name='react_app'),
-]
